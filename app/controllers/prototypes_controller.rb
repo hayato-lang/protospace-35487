1902e20e0ae02c5882b7f52392e58c5f48c71433
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-12 12:37:26 +0900

    Contoroller#updateの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,7 +32,7 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
       redirect_to action: 'show'
     else
       render :edit

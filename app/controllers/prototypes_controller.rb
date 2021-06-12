f56510aa0cf05f930304b3e335a3ae175d1a688a
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-12 14:55:32 +0900

    moce_to _indexの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!,  only: [:new, :edit, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
 
   def index
@@ -51,6 +51,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
+    @prototype = Prototype.find(params[:id])
     unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
